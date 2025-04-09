99277634ab4a0e4a8b9e23dbe41b6ec4cc103b5c
ziwonii24 <ziwonii25@gmail.com>
2025-04-09 16:34:54 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `One Dev Blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}

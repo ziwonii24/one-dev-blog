68fa0a25e97524f41f30b4fd1e3bae898ced2b25
ziwonii24 <ziwonii25@gmail.com>
2025-04-10 22:14:44 +0900

    :heavy_plus_sign: ADD shadcn/ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Seo from "../components/seo";
 import Layout from "../components/layout";
-
+import { Button } from "@/components/ui/button";
 const TITLE = "Home Page";
 
 const IndexPage = () => {
@@ -9,6 +9,7 @@ const IndexPage = () => {
     <Layout pageTitle={TITLE}>
       <p>This is Home Page.</p>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
     </Layout>
   );
 };

0d8933eab02119270f9e8698a9471b7b2516fc11
ziwonii24 <ziwonii25@gmail.com>
2025-04-23 22:38:50 +0900

    :lipstick: ADD h1 ui style

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -13,8 +13,10 @@ const Layout = ({
     <div className="border border-solid flex flex-col items-center h-screen">
       <Header />
       <div className="flex flex-col items-center w-full overflow-y-auto">
-        <main className="border border-solid border-black w-full max-w-2xl min-h-screen">
-          <h1>{pageTitle}</h1>
+        <main className="w-full max-w-2xl min-h-screen py-6">
+          <h1 className="text-3xl font-bold tracking-tight mb-2">
+            {pageTitle}
+          </h1>
           {children}
         </main>
         <Footer />
